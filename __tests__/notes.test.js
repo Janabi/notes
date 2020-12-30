d5fe14dcf801e98a9cc6d57c358030fc59daf7e3
Abd Alrhman <abdalrhman@Abds-MacBook-Pro.local>
2020-12-30 07:22:43 +0200

    lab 3 done

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,20 +1,31 @@
 'use strict';
 
 const Note = require('../lib/notes');
+const supergoose = require('supergoose');
+const mongoose = require('mongoose');
 
+
+
+jest.mock('supergoose');
+supergoose.mockImplementation(()=>{
+    return {
+        text: 'Hello From ASAC',
+        category: 'school'
+    }
+});
 jest.spyOn(global.console, 'log');
 
 describe('Note Module', ()=> {
 
     it('add() does nothing with invalid note', ()=> {
         const note = new Note();
-        note.add({payload: true});
+        note.add({text: true, category: true});
         expect(console.log).not.toHaveBeenCalled();
     });
 
     it('add() logs out result when given note', ()=> {
         const note = new Note();
-        note.add({payload: "Hello"});
+        note.add({text: 'Hello From ASAC', category: 'school'});
         expect(console.log).toHaveBeenCalled();
     });
 
--- index.js
+++ index.js
@@ -2,10 +2,30 @@
 
 'use strict';
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGOOSE_URI = process.env.MONGOOSE_URI;
+
+mongoose.connect(MONGOOSE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+
 const Input = require('./lib/input');
 const Note = require('./lib/notes');
 
 const input = new Input();
 const note = new Note();
 console.log('--------------------');
-note.execute(input);
+
+async function doExecuting() {
+    await note.execute(input);
+    console.log("Done!")
+    mongoose.disconnect();
+}
+
+doExecuting()
